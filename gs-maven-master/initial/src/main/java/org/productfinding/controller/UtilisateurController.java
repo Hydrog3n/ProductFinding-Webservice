5036f557bcd54e3eba4346af3e883844931e05e7
hydrog3n <loic.vaille@gmail.com>
2016-01-15 19:24:04 +0100

    fix : nullpointer User.

--- gs-maven-master/initial/src/main/java/org/productfinding/controller/UtilisateurController.java
+++ gs-maven-master/initial/src/main/java/org/productfinding/controller/UtilisateurController.java
@@ -2,37 +2,36 @@ package org.productfinding.controller;
 
 import org.productfinding.entity.Utilisateur;
 import org.productfinding.repository.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * Created by alehmann on 15/01/2016.
- */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/utilisateur")
 public class UtilisateurController {
 
-    private UtilisateurRepository userRepositoy;
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public Iterable<Utilisateur> getAll() {
-        return userRepositoy.findAll();
+        return utilisateurRepository.findAll();
     }
 
     @RequestMapping(value="/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public Utilisateur get(@PathVariable("id") Long id) {
-        return userRepositoy.findOne(id);
+        return utilisateurRepository.findOne(id);
     }
 
     @RequestMapping(value="/{id}", method = RequestMethod.PATCH, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     @Transactional
     public Utilisateur patch(@PathVariable("id") Long id, @RequestBody Utilisateur utilisateur) {
-        Utilisateur databaseUtilisateur = userRepositoy.findOne(id);
+        Utilisateur databaseUtilisateur = utilisateurRepository.findOne(id);
         if (utilisateur.getUsername() != null) {
             databaseUtilisateur.setUsername(utilisateur.getUsername());
         }
@@ -45,20 +44,13 @@ public class UtilisateurController {
         if (utilisateur.getLastName() != null) {
             databaseUtilisateur.setLastName(utilisateur.getLastName());
         }
-        return userRepositoy.save(databaseUtilisateur);
+        return utilisateurRepository.save(databaseUtilisateur);
     }
 
     @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable("id") Long id) {
-        userRepositoy.delete(id);
+        utilisateurRepository.delete(id);
     }
 
-    public UtilisateurRepository getUserRepositoy() {
-        return userRepositoy;
-    }
-
-    public void setUserRepositoy(UtilisateurRepository userRepositoy) {
-        this.userRepositoy = userRepositoy;
-    }
 }
