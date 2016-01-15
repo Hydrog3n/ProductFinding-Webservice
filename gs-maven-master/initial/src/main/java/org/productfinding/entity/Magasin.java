c47a4d9554003dae01ae90dac1ac40b5c8c69090
hydrog3n <loic.vaille@gmail.com>
2016-01-15 16:04:13 +0100

    Some change

--- gs-maven-master/initial/src/main/java/org/productfinding/entity/Magasin.java
+++ gs-maven-master/initial/src/main/java/org/productfinding/entity/Magasin.java
@@ -1,8 +1,28 @@
 package org.productfinding.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Magasin {
-    
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
 }
